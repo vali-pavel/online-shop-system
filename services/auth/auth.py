8ff8cd711ad8a10b1e79dd6b8151bc1f1fd53133
vali-pavel <valeriu.pavel92@gmail.com>
2022-03-09 19:06:01 +0200

    create the Auth class implement methods for creating and validating the auth token

---
+++ services/auth/auth.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from dotenv import load_dotenv
+import os, jwt
+
+load_dotenv()
+
+ALGORITHM = os.environ["ALGORITHM"]
+
+
+@dataclass
+class Auth:
+    def create_access_token(
+        user_id: int,
+        role: int,
+        secret_key: str,
+    ):
+        to_encode = {
+            "sub": user_id,
+            "role": role,
+        }
+
+        encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=ALGORITHM)
+        return encoded_jwt
+
+    def decode_access_token(access_token: str):
+        SECRET_KEY = os.environ["SECRET_KEY"]
+        try:
+            return jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+        except Exception as err:
+            return err
