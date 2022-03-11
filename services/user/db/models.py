fbb1176c1b620cc419e80fac90ae36b369f4c6a3
vali-pavel <valeriu.pavel92@gmail.com>
2022-03-11 17:05:20 +0200

    specify the String length for the columns inside User model

--- services/user/db/models.py
+++ services/user/db/models.py
@@ -7,8 +7,8 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, autoincrement=True, primary_key=True)
-    full_name = Column(String, nullable=False)
-    email = Column(String, nullable=False)
+    full_name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
     phone_number = Column(Integer, nullable=False)
     role_type = Column(Integer, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    hashed_password = Column(String(100), nullable=False)
