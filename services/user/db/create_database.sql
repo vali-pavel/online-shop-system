2ffefdc1cdf5374060d1818eda13cc4545b6dbfc
vali-pavel <valeriu.pavel92@gmail.com>
2022-03-11 16:54:59 +0200

    better column names for the user table

--- services/user/db/create_database.sql
+++ services/user/db/create_database.sql
@@ -5,10 +5,10 @@ USE users;
 GO
 
 CREATE TABLE IF NOT EXISTS user (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     phone_number INT NOT NULL,
-    user_role INT NOT NULL,
-    user_password VARCHAR(150) NOT NULL
+    role_type INT NOT NULL,
+    hashed_password VARCHAR(150) NOT NULL
 );
