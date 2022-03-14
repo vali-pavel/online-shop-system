2ccfaf51c0eb1d6775513f3357b17cc692b9df6d
vali-pavel <valeriu.pavel92@gmail.com>
2022-03-14 15:03:06 +0200

    set the ORM mode to user schemas

--- services/user/api/schemas.py
+++ services/user/api/schemas.py
@@ -4,12 +4,18 @@ from pydantic import BaseModel, EmailStr, Field
 class UserBase(BaseModel):
     email: EmailStr
 
+    class Config:
+        orm_mode = True
+
 
 class UserCreate(UserBase):
     full_name: str = Field(...)
     phone_number: int = Field(...)
     role_type: int = Field(...)
     password: str = Field(...)
+
+    class Config:
+        orm_mode = True
 
 
 class UserLogin(UserBase):
