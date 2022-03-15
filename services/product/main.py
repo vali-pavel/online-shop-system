95d4d2e6eca3253dcb104fbe9d955237fda7b217
vali-pavel <valeriu.pavel92@gmail.com>
2022-03-15 14:33:07 +0200

    add the main.py file

---
+++ services/product/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+
+from api import api
+from api.middlewares import AuthMiddleware
+
+app = FastAPI()
+app.add_middleware(AuthMiddleware)
+app.include_router(api.router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8003)
